40938152e23275ac610094f7520e906e47add6db
Lvschoor <lvschoor@gmail.com>
2021-12-23 15:03:14 +0100

    Adjusted for problem with nested list, reading from csv.

--- opdracht-4.py
+++ opdracht-4.py
@@ -4,16 +4,14 @@ my_csv_file = './resources/data.csv'
 products = ['appel', 'aap', 'opa', 'appel', 5, True, 5, 7, 'appel']
 
 # writing data from products to csv
-with open(my_csv_file, 'w',) as file:
+with open(my_csv_file, 'w', ) as file:
     writer = csv.writer(file)
     writer.writerow(products)
-
 
 # reading data from csv file into list
 with open(my_csv_file) as file:
     reader = csv.reader(file)
-    list_csv = list(reader)
-print(list_csv)
+    list_csv = next(reader)
 
 
 def frequency(list_to_be_analyzed):
